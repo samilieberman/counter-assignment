a4f8b26cbcfede464c4a4c414f152abd2a94c7fa
pyagnik12 <112579379+pyagnik12@users.noreply.github.com>
2023-04-05 14:47:39 -0400

    React Testing
    wrote React tests to make sure all the components are working the way they should.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -12,8 +12,8 @@ export default function Counter() {
     <div>
       <h2>Counter</h2>
       <p data-testid="count">{count}</p>
-      <button onClick={increment}> + </button>
-      <button onClick={decrement}> - </button>
+      <button onClick={increment}> Add </button>
+      <button onClick={decrement}> Subtract </button>
     </div>
   );
 }
--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,31 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Counter from '../components/Counter';
 
 beforeEach(() => {
-  // Render the Counter component here
-})
+  render(<Counter />);
+});
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  const counterMessage = screen.getByText(/Counter/i);
+  expect(counterMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+  const countElement = screen.getByTestId('count');
+  expect(countElement).toHaveTextContent('0');
 });
 
 test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const addButton = screen.getByRole('button', { name: /add/i });
+  userEvent.click(addButton);
+  const countElement = screen.getByTestId('count');
+  expect(countElement).toHaveTextContent('1');
 });
 
 test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  const subtractButton = screen.getByRole('button', { name: /subtract/i });
+  userEvent.click(subtractButton);
+  const countElement = screen.getByTestId('count');
+  expect(countElement).toHaveTextContent('-1');
 });
