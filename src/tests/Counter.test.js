d78722b6ab75521174224933daeea0a923e4d486
neevasethi <sethineeva@gmail.com>
2023-04-09 15:22:05 -0400

    counter asignment testing code updated

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,37 @@
 // import necessary react testing library helpers here
 // import the Counter component here
+import {render,screen,fireEvent} from '@testing-library/react';
+import Counter from '../components/Counter';
 
 beforeEach(() => {
   // Render the Counter component here
+  render(<Counter />);
 })
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
+	const welcomeMessage = screen.getByText(/Counter/i);
+	expect(welcomeMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
   // Complete the unit test below based on the objective in the line above
+	const countVar = screen.getByTestId('count');
+	expect(countVar).toHaveTextContent("0");
 });
 
 test('clicking + increments the count', () => {
   // Complete the unit test below based on the objective in the line above
+	const countVariable = screen.getByTestId('count');
+	const plusButton = screen.getByText('+');
+	fireEvent.click(plusButton);
+	expect(countVariable).toHaveTextContent("1");
 });
 
 test('clicking - decrements the count', () => {
   // Complete the unit test below based on the objective in the line above
+	const countVar = screen.getByTestId('count');
+	const minusButton = screen.getByText('-');
+	fireEvent.click(minusButton);
+	expect(countVar).toHaveTextContent("-1");
 });
