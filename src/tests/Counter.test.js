12873c2b43b7ef08f5035e9d5691b578f23c8019
Jacob Leverock <greenops@comcast.net>
2023-11-14 12:18:35 -0500

    Testing 100% complete

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,34 @@
 // import necessary react testing library helpers here
 // import the Counter component here
+import { render, screen, fireEvent } from '@testing-library/react';
+import Counter from '../components/Counter';
+import React from 'react';
 
 beforeEach(() => {
   // Render the Counter component here
+  render(<Counter />);
 })
+
+// react testing commands - https://testing-library.com/docs/dom-testing-library/cheatsheet
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
+  expect(screen.getByText(/counter/i)).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
   // Complete the unit test below based on the objective in the line above
+  expect(screen.getByTestId('count')).toHaveTextContent('0');
 });
 
 test('clicking + increments the count', () => {
   // Complete the unit test below based on the objective in the line above
+  fireEvent.click(screen.getByText('+'));
+  expect(screen.getByTestId('count')).toHaveTextContent('1');
 });
 
 test('clicking - decrements the count', () => {
   // Complete the unit test below based on the objective in the line above
+  fireEvent.click(screen.getByText('-'));
+  expect(screen.getByTestId('count')).toHaveTextContent('-1');
 });
