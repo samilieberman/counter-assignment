348e1fc87eed892a9f92605054ad5464674847e4
nfract <nfractt@gmail.com>
2023-11-16 15:06:28 -0500

    Added testing for counter component rendering and testing for counter component increment / decrement.

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,24 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import { fireEvent, render, screen } from '@testing-library/react';
+import Counter from '../components/Counter';
 
 beforeEach(() => {
-  // Render the Counter component here
+  render(<Counter />);
 })
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  expect(screen.getByText(/Counter/i)).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+  expect(screen.getByText(/0/i)).toBeInTheDocument();
 });
 
 test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  fireEvent.click(screen.getByRole('button', {name: '+'}));
+  expect(screen.getByText(/1/i)).toBeInTheDocument();
 });
 
 test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  fireEvent.click(screen.getByRole('button', {name: '-'}));
+  expect(screen.getByText(/-1/i)).toBeInTheDocument();
 });
