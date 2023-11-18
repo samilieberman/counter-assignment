ee9a7013da2e2f1e69a451aadd47e16c844ff6a0
maingotj <maingotj@ufl.edu>
2023-11-18 17:25:44 -0500

    Added Counter tests

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,28 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import { render, fireEvent, screen, getByTestId } from "@testing-library/react";
+import Counter from "../components/Counter";
 
 beforeEach(() => {
-  // Render the Counter component here
-})
-
-test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  render(<Counter />);
 });
 
-test('should render initial count with value of 0', () => {
+test("renders counter message", () => {
   // Complete the unit test below based on the objective in the line above
+  expect(screen.getByText(/counter/i)).toBeInTheDocument;
 });
 
-test('clicking + increments the count', () => {
+test("should render initial count with value of 0", () => {
   // Complete the unit test below based on the objective in the line above
+  expect(screen.getByTestId('count')).toHaveTextContent("0");
 });
 
-test('clicking - decrements the count', () => {
+test("clicking + increments the count", () => {
   // Complete the unit test below based on the objective in the line above
+  fireEvent.click(screen.getByText("+"));
+  expect(screen.getByText(/1/));
+});
+
+test("clicking - decrements the count", () => {
+  // Complete the unit test below based on the objective in the line above
+  fireEvent.click(screen.getByText("-"));
+  expect(screen.getByText(/-1/));
 });
