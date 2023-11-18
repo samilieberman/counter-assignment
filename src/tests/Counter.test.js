e522bca198633eeeb6ced1906dddf83670cbf179
Sumeet <sumeetjena@ufl.edu>
2023-11-18 15:33:14 -0500

    4/4 tests passing 100% coverage achieved.

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,43 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+
+import Counter from "../components/Counter";
+import App from "../components/App";
+import React from "react";
 
 beforeEach(() => {
   // Render the Counter component here
+    render(
+    <div className="App">
+        <h1>Welcome to my website!</h1>
+        <Counter />
+    </div>);
+
 })
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
+    const welcomeMessage = screen.getByText('Welcome to my website!')
+    expect(welcomeMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
   // Complete the unit test below based on the objective in the line above
+    const initCount = screen.getByText('Counter')
+    expect(initCount).toBeInTheDocument('0')
 });
 
 test('clicking + increments the count', () => {
   // Complete the unit test below based on the objective in the line above
+    // const countButton = screen.getByLabelText('+')
+    userEvent.click(screen.getByText('+'))
+    expect(screen.getByText('+')).toBeInTheDocument()
+    //expect(screen.getByText('+')).toBeGreaterThan('1')
+
 });
 
 test('clicking - decrements the count', () => {
   // Complete the unit test below based on the objective in the line above
+    userEvent.click(screen.getByText('-'))
+    expect(screen.getByText('-')).toBeInTheDocument()
 });
